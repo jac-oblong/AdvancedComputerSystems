c4ff6383b53a17b37b637a12c2a7c3c5fb16b174
Jacob Long <jacoblong853@gmail.com>
2024-10-18 23:01:51 -0400

    add parsing of fio outputs

--- SSDPerformanceProfiling/run.py
+++ SSDPerformanceProfiling/run.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 from pathlib import Path
+import json
 
 SCRIPT_DIR = Path(__file__).parent
 OUTPUT_DIR = SCRIPT_DIR / "output"
@@ -14,7 +15,7 @@ READ_PERCENTAGES = [0, 50, 70, 100]
 
 def run(iodepth, block_size, read_percent):
     out_file = OUTPUT_DIR / f"output_qlen{iodepth}_bs{block_size}_rw{read_percent}.json"
-    job_file = SCRIPT_DIR / "jobs.fio"
+    job_file = SCRIPT_DIR / "fio.ini"
     cmd = [
         "fio",
         "--output-format=json",
@@ -28,12 +29,43 @@ def run(iodepth, block_size, read_percent):
     subprocess.run(cmd, encoding="utf-8")
 
 
+def parse(file_name, result):
+    segments = file_name.split(".")[0].split("_")[1:]
+    qlen = int(segments[0][4:])
+    bs = int(segments[1][2:])
+    rw = int(segments[2][2:])
+
+    stats = [
+        result["jobs"][0]["read"]["bw"],
+        result["jobs"][0]["read"]["iops"],
+        result["jobs"][0]["read"]["lat_ns"]["mean"],
+        result["jobs"][0]["write"]["bw"],
+        result["jobs"][0]["write"]["iops"],
+        result["jobs"][0]["write"]["lat_ns"]["mean"],
+    ]
+    stats = [str(s) for s in stats]
+
+    return f"{bs}, {rw}:{100-rw}, {qlen}, " + ", ".join(stats)
+
+
 try:
     os.mkdir(OUTPUT_DIR)
 except FileExistsError:
     pass
 
 
+# run all the necessary tests
 combined = [(d, b, r) for d in IO_DEPTHS for b in BLOCK_SIZES for r in READ_PERCENTAGES]
 for depth, bs, read in combined:
     run(depth, bs, read)
+
+# parse the results of all the tests
+print(
+    "Data Access Size, RW Ratio, IO queue depth,"
+    + " Read Bandwidth (KB/s), Read IOPS, Read Latency (ns),"
+    + " Write Bandwidth (KB/s), Write IOPS, Write Latency (ns)"
+)
+for file in OUTPUT_DIR.glob("*.json"):
+    with file.open("r", encoding="utf-8") as fp:
+        data = json.load(fp)
+    print(parse(file.name, data))
